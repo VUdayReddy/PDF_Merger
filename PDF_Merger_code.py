8855b8e4f77e75ae8bf617166ee8d7d641c9eb87
Uday Reddy Vangala <VUdayReddy@users.noreply.github.com>
2020-05-15 11:52:35 +0530

    Update PDF_Merger_code.py

--- PDF_Merger_code.py
+++ PDF_Merger_code.py
@@ -7,6 +7,6 @@ def PDF_Merger(input):
   merger = PyPDF2.PdfFileMerger()
   for file in input:
     merger.append(file)
-  merger.write('mereged_pdf.pdf')
+  merger.write('merged_pdf.pdf')
  
 PDF_Merger(input_files)
